c26970897ec7920ac5bc455018547ef494290f76
unknown <truepace24@gmail.com>
2024-04-25 23:22:41 +0100

    first commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,18 +7,20 @@ const page =  () => {
   
   return (
     <>
-   <div class="h-screen overflow-scroll whitespace-nowrap   snap-both ">
+   <div class="h-screen overflow-scroll whitespace-nowrap   snap-both snap-mandatory ">
   <div class=" bg-blue-500 h-full snap-start inline-block w-full">Item 1</div>
   <div class=" bg-green-500 h-full snap-start inline-block w-full">Item 2</div>
   <div class=" bg-red-500 h-full snap-start inline-block w-full">Item 3</div>
-  <div class=" bg-green-500 h-full snap-start inline-block w-full">Item 2</div>
-  <div class=" bg-red-500 h-full snap-start inline-block w-full">Item 3</div>
+  <div class=" bg-yellow-300 h-full snap-start inline-block w-full">Item 4</div>
+  <div class=" bg-purple-500 h-full snap-start inline-block w-full">Item 5</div>
   <br />
   
-  <div class=" bg-green-500 h-full snap-start inline-block w-full">Item 2</div>
-  <div class=" bg-red-500 h-full snap-start inline-block w-full">Item 3</div>
-  <div class=" bg-green-500 h-full snap-start inline-block w-full">Item 2</div>
-  <div class=" bg-red-500 h-full snap-start inline-block w-full">Item 3</div>
+  <div class=" bg-gray-500 h-full snap-start inline-block w-full">Item 12</div>
+  {/* <div class=" bg-orange-600 h-full snap-start inline-block w-full">Item 13</div>
+  <div class=" bg-amber-950 h-full snap-start inline-block w-full">Item 14</div>
+  <div class=" bg-indigo-200 h-full snap-start inline-block w-full">Item 15</div> */}
+  <br/>
+  <div class=" bg-orange-600 h-full snap-start inline-block w-full">Item 13</div>
   
 </div>
 
