946df92ac1c6b90d2bdf25eab7f712a669321f3f
unknown <truepace24@gmail.com>
2024-05-11 23:48:44 +0100

    maybe

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,15 +20,29 @@ const page =  () => {
   <Tablet/>
   
   </div>
-  <div class=" bg-green-500 h-full snap-start inline-block w-full">Item 2</div>
-  <div class=" bg-red-500 h-full snap-start inline-block w-full">Item 3</div>
-  <div class=" bg-yellow-300 h-full snap-start inline-block w-full">Item 4</div>
+  <div class=" bg-green-500 h-full snap-start inline-block w-full">
+  <Tablet/>
+  </div>
+  <div class=" bg-red-500 h-full snap-start inline-block w-full">
+  <Tablet/>
+  </div>
+  <div class=" bg-yellow-300 h-full snap-start inline-block w-full">
+  <Tablet/>
+  </div>
   <div class=" bg-purple-500 h-full snap-start inline-block w-full">Item 5</div>
- 
-  <div class=" bg-black h-full snap-start inline-block w-full">Item 2</div>
-  <div class=" bg-zinc-700 h-full snap-start inline-block w-full">Item 3</div>
-  <div class=" bg-blue-400 h-full snap-start inline-block w-full">Item 4</div>
-  <div class=" bg-cyan-900 h-full snap-start inline-block w-full">Item 5</div>
+  <Tablet/>
+  <div class=" bg-black h-full snap-start inline-block w-full">
+  <Tablet/>
+  </div>
+  <div class=" bg-zinc-700 h-full snap-start inline-block w-full">
+  <Tablet/>
+  </div>
+  <div class=" bg-blue-400 h-full snap-start inline-block w-full">
+  <Tablet/>
+  </div>
+  <div class=" bg-cyan-900 h-full snap-start inline-block w-full">
+  <Tablet/>
+  </div>
   
 </div>
 
