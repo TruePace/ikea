5298199f940d0b5c8d67ad5b3d03b7f889f64365
unknown <truepace24@gmail.com>
2024-08-21 03:12:45 +0100

    why

--- src/components/Headline_news_comps/Tabs/Headline_Tabs_Comps/ContentFeed.jsx
+++ src/components/Headline_news_comps/Tabs/Headline_Tabs_Comps/ContentFeed.jsx
@@ -1,18 +1,28 @@
 import Image from "next/image";
 
-const ContentFeed = ({content}) => {
-    return (
-        <>
-        <div key={content._id}>
-          <p className="text-md text-gray-600 mt-2 ">{content.message}</p>
-<div className=" relative  border-red-400 h-64 mt-2 ">{/*border-2  removed*/}
-        <Image src={content.picture} fill alt="Picture of the author " className="rounded-md object-cover"/>
-        </div>
-
-
-        </div>
-        </>
-    );
-}
+const ContentFeed = ({ content }) => {
+  return (
+    <>
+      <div key={content._id}>
+        {content.picture ? (
+          // Styling when both message and picture are present
+          <div className="flex flex-col gap-2">
+            <div className=" xss:text-sm  xs:p-2 xs:leading-7  sm:py-2 sm:px-2 sm:leading-8 sm:text-lg text-md  text-gray-600  capitalize rounded-md">
+              {content.message}
+            </div>
+            <div className="relative border-red-400 mt-1 xs:h-56 sm:h-60 xss:py-16 xss:mt-0 xss:text-xs">
+              <Image src={content.picture} fill alt="Picture of the author" className="rounded-md object-cover" />
+            </div>
+          </div>
+        ) : (
+          // Styling when only message is present
+          <div className="xss:text-xs xss:py-3 xss:leading-5 xs:py-14 xs:text-lg sm:py-16  sm:text-lg  sm:leading-8 text-md text-gray-600 mt-2   w-full   flex items-center   capitalize justify-center ">
+            {content.message}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default ContentFeed;
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,17 @@ module.exports = {
       'sans':['sohne','Helvetica' ,'Neue','Helvetica', 'Arial', 'sans-serif'],
       'serif':['Segoe UI','Roboto','Oxygen','Ubuntu','Cantarell','Open Sans','Helvetica Neue','sans-serif']
     },
+    screens:{
+      'xss': {'min': '161px', 'max': '320px'},
+      // => @media (min-width: 161px and max-width: 320px) { ... }
+
+      'xs': {'min': '321px', 'max': '390px'},
+      // => @media (min-width: 321px and max-width: 640px) { ... }
+      'sm': {'min': '391px', 'max': '576px'},
+      // => @media (min-width: 401px and max-width: 576px) { ... }
+      'md': {'min': '577px', 'max': '640px'},
+      // => @media (min-width: 577px and max-width: 640px) { ... }
+    },
   },
   plugins: [
     require("daisyui"),
