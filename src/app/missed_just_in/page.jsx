ea3226cecf306c2a78bd9bd6d897884e1c71d155
unknown <truepace24@gmail.com>
2024-10-21 23:21:00 +0100

    okay

--- src/app/missed_just_in/page.jsx
+++ src/app/missed_just_in/page.jsx
@@ -78,7 +78,7 @@ const Page = () => {
         ) : (
           <div className="bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4 rounded" role="alert">
             <p className="font-bold">Good news!</p>
-            <p>You haven&apos;t missed any Just In content. You're all caught up!</p>
+            <p>You haven&apos;t missed any Just In content. You&apos;re all caught up!</p>
           </div>
         )}
       </div>
