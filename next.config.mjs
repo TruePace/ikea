73d2bf046043b1564ebe209e68f5243fc1916155
unknown <truepace24@gmail.com>
2024-10-21 23:50:47 +0100

    okay

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    swcMinify: true,
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
   images: {
     domains: ['lh3.googleusercontent.com'],
     remotePatterns: [
