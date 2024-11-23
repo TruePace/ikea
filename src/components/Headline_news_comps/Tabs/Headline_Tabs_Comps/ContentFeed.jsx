972d632257b0e5599dd63bbe7c49359293cc5ef0
unknown <truepace24@gmail.com>
2024-11-23 19:03:29 +0100

    okay

--- src/components/Headline_news_comps/Tabs/Headline_Tabs_Comps/ContentFeed.jsx
+++ src/components/Headline_news_comps/Tabs/Headline_Tabs_Comps/ContentFeed.jsx
@@ -25,24 +25,49 @@ const ContentFeed = ({ content, onView, isViewed }) => {
     };
   }, [onView, isViewed]);
 
+  const renderTags = (tags) => {
+    return tags.map((tag, index) => (
+      <span 
+        key={index} 
+        className="inline-block bg-blue-100 text-blue-800 text-sm px-2 py-1 rounded-full mr-2 mb-2"
+      >
+        #{tag}
+      </span>
+    ));
+  };
+
   return (
-    <div ref={contentRef} id={`content-${content._id}`} className="w-full flex flex-col mt-1 gap-8  ">
-      {content.picture ? (
-        <>
-          <div className="xs:pb-5 sm:pb-5 md:pb-5   text-sm sm:text-[17px] sm:mt-2 xs:leading-5 leading-6 tablet:text-base desktop:text-lg dark:text-white text-gray-700 capitalize rounded-md mb-2 desktop:mb-4 flex-grow overflow-y-auto   ">
-            {content.message}
-          </div>
-          <div className="relative border-red-400 h-60 tablet:h-72 desktop:h-80 flex-shrink-0 ">
-            <Image src={content.picture} fill alt="Content image" className="rounded-md object-cover" />
-          </div>
-        </>
-      ) : (
-        <div className="h-96 tablet:h-108 desktop:h-120  text-gray-700 w-full flex items-center capitalize dark:text-gray-200">
-          <p className="text-sm sm:text-[17px] xs:leading-5 leading-6  tablet:text-base desktop:text-lg">{content.message}</p>
+    <div ref={contentRef} id={`content-${content._id}`} className="w-full flex flex-col mt-1 gap-8">
+    {content.picture ? (
+      <>
+        <div className="xs:pb-5 sm:pb-5 md:pb-5 text-sm sm:text-[17px] sm:mt-2 xs:leading-5 leading-6 tablet:text-base desktop:text-lg dark:text-white text-gray-700 capitalize rounded-md mb-2 desktop:mb-4 flex-grow overflow-y-auto">
+          {content.message}
         </div>
-      )}
-    </div>
-  );
+        <div className="relative border-red-400 h-60 tablet:h-72 desktop:h-80 flex-shrink-0">
+          <Image src={content.picture} fill alt="Content image" className="rounded-md object-cover" />
+        </div>
+        {content.tags && content.tags.length > 0 && (
+          <div className="mt-2">
+            {renderTags(content.tags)}
+          </div>
+        )}
+      </>
+    ) : (
+      <>
+        <div className="h-96 tablet:h-108 desktop:h-120 text-gray-700 w-full flex flex-col capitalize dark:text-gray-200">
+          <p className="text-sm sm:text-[17px] xs:leading-5 leading-6 tablet:text-base desktop:text-lg">
+            {content.message}
+          </p>
+          {content.tags && content.tags.length > 0 && (
+            <div className="mt-4">
+              {renderTags(content.tags)}
+            </div>
+          )}
+        </div>
+      </>
+    )}
+  </div>
+);
 };
 
 export default ContentFeed;
